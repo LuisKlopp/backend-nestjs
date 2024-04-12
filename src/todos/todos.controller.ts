a9971ef3966461e4db21f343916ed1bb5f7e6875
LuisKlopp <fbgus333@gmail.com>
2024-04-12 23:02:08 +0900

    Feat: 투두리스트 CRUD API완성

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { Todo } from './entities/todo.entity';
 
 @Controller('todos')
 export class TodosController {
@@ -8,5 +17,20 @@ export class TodosController {
   @Get()
   getAll() {
     return this.todosService.findAll();
+  }
+
+  @Post()
+  async create(@Body() todoData: Todo): Promise<Todo> {
+    return this.todosService.create(todoData);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    return this.todosService.delete(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() todoData: Todo): Promise<Todo> {
+    return this.todosService.update(id, todoData);
   }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -18,8 +18,17 @@ export class TodosService {
     return this.todoRepository.find();
   }
 
-  // async create(todoData: Todo): Promise<Todo> {
-  //   const newTodo = this.todoRepository.create(todoData);
-  //   return await this.todoRepository.save(newTodo);
-  // }
+  async create(todoData: Todo): Promise<Todo> {
+    const newTodo = this.todoRepository.create(todoData);
+    return this.todoRepository.save(newTodo);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.todoRepository.delete(id);
+  }
+
+  async update(id: number, todoData: Todo): Promise<Todo> {
+    await this.todoRepository.update(id, todoData);
+    return this.todoRepository.findOne({ where: { id: id } });
+  }
 }
