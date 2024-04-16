4570f333c2751ee177535d91e86adc1baf8ac576
LuisKlopp <fbgus333@gmail.com>
2024-04-16 18:27:19 +0900

    Feat: comment 조회 api 생성

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { Comment } from './entities/comment.entity';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  constructor(private readonly commentsService: CommentsService) {}
+
+  @Get('posts/:postId')
+  async getComments(@Param('postId') postId: number): Promise<Comment[]> {
+    return this.commentsService.getComments(postId);
+  }
+}
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>,
+  ) {}
+
+  async getComments(postId: number): Promise<Comment[]> {
+    return await this.commentRepository.find({ where: { post_id: postId } });
+  }
+}
