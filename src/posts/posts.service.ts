38152b6dc83a31d22a78fe58ffae5e1f6ea66cc2
LuisKlopp <fbgus333@gmail.com>
2024-04-16 15:31:14 +0900

    Fix: Like API 리턴값 제거

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -14,18 +14,17 @@ export class PostsService {
     return await this.postRepository.findOne({ where: { urls: slug } });
   }
 
-  async likePost(id: number): Promise<PostEntity> {
+  async likePost(id: number): Promise<void> {
     const post = await this.postRepository.findOneBy({ id });
     post.likes += 1;
-    return this.postRepository.save(post);
+    await this.postRepository.save(post);
   }
 
-  async unlikePost(id: number) {
+  async unlikePost(id: number): Promise<void> {
     const post = await this.postRepository.findOneBy({ id });
     if (post && post.likes > 0) {
       post.likes -= 1;
       await this.postRepository.save(post);
     }
-    return post;
   }
 }
