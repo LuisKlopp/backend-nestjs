b6c0ebb4d904a1718740fd12c00d4f2c4e9275a1
LuisKlopp <fbgus333@gmail.com>
2024-04-28 19:28:23 +0900

    Fix: comments dto 수정

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateCommentDto {
   @IsString()
@@ -8,4 +8,8 @@ export class CreateCommentDto {
   @IsString()
   @IsNotEmpty()
   readonly content: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly password: number;
 }
