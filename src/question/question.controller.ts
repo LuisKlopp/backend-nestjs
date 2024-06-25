1e658189cca96e61e79e0fa81e6bb5d2870bb2dd
LuisKlopp <fbgus333@gmail.com>
2024-06-26 00:23:34 +0900

    Feat: logger 로깅 확인

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -7,20 +7,24 @@ import {
   Body,
   HttpStatus,
   HttpCode,
+  Logger,
 } from '@nestjs/common';
 import { QuestionService } from './question.service';
 
 @Controller('question')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
+  private readonly logger = new Logger(QuestionController.name);
 
   @Get()
   async getQuestions() {
+    this.logger.log('리스트 API 호출됨');
     return this.questionService.getQuestions();
   }
 
   @Get(':id')
   async getQuestionById(@Param('id', ParseIntPipe) id: number) {
+    this.logger.log('상세페이지 API 호출됨');
     return this.questionService.getQuestionById(id);
   }
 
